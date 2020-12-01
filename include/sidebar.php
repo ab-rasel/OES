d78998a9ba7e30198982a1d6bb97959f8c77c0fb
Ab Rasel <39724326+abrasel600@users.noreply.github.com>
2020-12-01 21:40:18 +0600

    update

--- include/sidebar.php
+++ include/sidebar.php
@@ -30,8 +30,8 @@
         <li><a class="app-menu__item active" href="deshboard.php"><i class="app-menu__icon fa fa-dashboard"></i><span class="app-menu__label">Dashboard</span></a></li>
         <li class="treeview"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-users"></i><span class="app-menu__label">Teachers</span><i class="treeview-indicator fa fa-angle-right"></i></a>
           <ul class="treeview-menu">
-          <li><a class="treeview-item" href="#"><i class="icon fa fa-user-plus"></i>Register New Teacher</a></li>
-          <li><a class="treeview-item" href="#"><i class="icon fa fa-users"></i>View Teacher List</a></li>
+          <li><a class="treeview-item" href="teacher-create.php"><i class="icon fa fa-user-plus"></i>Register New Teacher</a></li>
+          <li><a class="treeview-item" href="teacher-show.php"><i class="icon fa fa-users"></i>View Teacher List</a></li>
            
           </ul>
         </li>
@@ -40,8 +40,8 @@
         <!--  -->
         <li class="treeview"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-book"></i><span class="app-menu__label">Course</span><i class="treeview-indicator fa fa-angle-right"></i></a>
           <ul class="treeview-menu">
-          <li><a class="treeview-item" href="#"><i class="icon fa fa-circle-o"></i> Create New Course</a></li>
-            <li><a class="treeview-item" href="#"><i class="icon fa fa-circle-o"></i>View Course List</a></li>
+          <li><a class="treeview-item" href="course-create.php"><i class="icon fa fa-circle-o"></i> Create New Course</a></li>
+            <li><a class="treeview-item" href="course-show.php"><i class="icon fa fa-circle-o"></i>View Course List</a></li>
             
           </ul>
         </li>
